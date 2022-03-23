a4e18d3f3d80335ff7127b22d22aee8eff7d73ff
nicocerezo09 <nico.cerezo@hotmail.com>
2022-03-23 18:03:16 -0300

    agregue un navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'./images/logo1.png'} className="App-logo" alt="logo" />
         <button>Hola nico!</button>
       </header>
     </div>
